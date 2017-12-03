a6db24657be11e610f3abcc4f4288c0b81a42927
SilverShadowx <ericsilver123@gmail.com>
2017-12-02 19:08:28 -0600

    added a test place for handling forms

--- test_server.js
+++ test_server.js
@@ -1,15 +1,36 @@
 var http = require("http");
 var url = require("url");
-var path = require("path");
 var fs = require("fs");
+var s = require("net").Socket();
+
 
 var server = http.createServer(function(req, resp) 
 {
     var uri = url.parse(req.url).pathname;
     console.log(req.url);
-	if(uri === "/BASIC.html")
+	//RootHTML page
+	
+	if(uri === "/")
 	{
-		fs.readFile("./BASIC.html", function (error, pgResp)
+		
+		fs.readFile("./Format.html", function (error, pgResp) {
+            console.log("currently in function Root");
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            }
+            else {
+                console.log("currently else or printing Root");
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+            resp.end();
+        });
+	}
+	//BasicHTML, for testing
+    else if(uri === "/BASIC.html") 
+	{
+        fs.readFile("./BASIC.html", function (error, pgResp)
 		{
 		    console.log("currently in function Basic");
 			if(error) {
@@ -23,25 +44,11 @@ var server = http.createServer(function(req, resp)
 			}
 		    resp.end();
 		});
-	}
-    else if (uri === "/") {
-        fs.readFile("./testing.html", function (error, pgResp) {
-            console.log("currently in function testing");
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            }
-            else {
-                console.log("currently else or printing Testing");
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-            resp.end();
-        });
     }
+	//TestHTML, for testing
     else if (uri === "/test") {
-        fs.readFile("./Format.html", function (error, pgResp) {
-            console.log("currently in function testing2");
+        fs.readFile("./testing.html", function (error, pgResp) {
+            console.log("currently in function Testing");
             if (error) {
                 resp.writeHead(404);
                 resp.write('Contents you are looking are Not Found');
@@ -54,9 +61,6 @@ var server = http.createServer(function(req, resp)
             resp.end();
         });
     }
-    else if (uri === "/favicon.ico") {
-	    console.log("Currently in Favicon.ico");
-	}
 	else
 	{
         console.log("interesting, second else: " + uri);
